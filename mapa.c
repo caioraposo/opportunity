e60e04aae6ae2dfbf1c463d1f74aca7c48dc940e
Caio Raposo <caioraposo94@gmail.com>
2019-04-01 12:23:12 -0300

    Finish robot colision

--- mapa.c
+++ mapa.c
@@ -1,48 +1,43 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include <conio.h>
 #include <math.h>
-#include <windows.h>
 #include <time.h>
 #include <unistd.h>
 
 int main(int argc, char** argv[]) {
-	
-	// Inicialização
 
+	// Inicialização
 	srand(time(0));
-	int x, y, v, a, g, p;
+	int x, y, v, a, g, p, parar; // Variaveis de cotrole
 	int jogadory = rand()%(9);//Posição do jogador no eixo x
 	int jogadorx = rand()%(19);//Posição do jogador no eixo y
-	int obs = rand()%(25);
-	int obstaculox[obs];//Criar lista dos obstáculos x 
-	int obstaculoy[obs];//Criar lista dos obstáculos y  
+	int obs = 50;
+	int obstaculox[obs];//Criar lista dos obstáculos x
+	int obstaculoy[obs];//Criar lista dos obstáculos y
 	char campo [10][20];//Criar campo
 	g = 0;
+	parar = 0;
 	// Criação do mapa
 
-	for(x = 0; x <= obs; x++){ // FOR OBSTACULOS 1 - Definição dos obstaculos no eixo X
+	for(x = 0; x < obs; x++){ // FOR OBSTACULOS 1 - Definição dos obstaculos no eixo X
 		obstaculox[x] = rand()%(9);
 	}
-	for (y = 0; y <= obs; y++) { // FOR OBSTACULOS 2 - Definição dos obstaculos no eixo Y
+	for (y = 0; y < obs; y++) { // FOR OBSTACULOS 2 - Definição dos obstaculos no eixo Y
 		obstaculoy[y] = rand()%(19);
 	}
-	
+
 	while (g == 0) { // While responsável por manter o jogo rodando
-		system("cls");
+		//system("clear");
+		printf("%d %d\n",jogadorx, jogadory );
 		for (x = 0; x < 10; x++) { // FOR 3 - Responsável pela impressão do mapa
-			
+
 			for (y = 0; y < 20;y++) { // FOR 4 - Definição da malha do mapa
-				if (campo[x][y] == '1') {
+				if (campo[x][y] == '1' || campo[x][y] == '#') {
 				}
 				else {
 					campo[x][y] = '0';
 				}
-				for (v = 0; v <= obs; v++) { // FOR 5 - Geração da indicação de obstáculos no mapa
-					if (obstaculox[v] == x && obstaculoy[v] == y) { // IF 1
-						campo[x][y] = '#';
-					} 
-				}
+
 				if (jogadorx == y && jogadory == x) { // IF 2 - Indicação do jogador no mapa
 					campo[x][y] = 'x';
 				}
@@ -52,7 +47,7 @@ int main(int argc, char** argv[]) {
 				}
 			}
 		}
-		Sleep(1000);
+
 		for (a = 0; a == 0;) { // Controle de erros durante a inserção das coordenadas
 			char dir;
 			int n;
@@ -61,9 +56,34 @@ int main(int argc, char** argv[]) {
 			if (n > 0) {
 				switch (dir) {
 					case 'N':
-						for (p = 0; p < n; p++) {
+						for (p = 0; p < n;) {
+
 							campo[jogadory][jogadorx] = '1';
 							jogadory--;
+							printf("subtraind y\n");
+							printf("%d\n",n);
+							p++;
+							fflush;
+
+							for(x = 0; x < obs; x++){
+								printf("Jogadorx %d\nJogadory %d\nObstaculox %d\nObstaculoy %d\n\n",jogadorx, jogadory, obstaculox[x], obstaculoy[x] );
+								if (jogadory == obstaculox[x] && jogadorx == obstaculoy[x]) {
+									printf("1234");
+									parar = 1;
+									campo[obstaculox[x]][obstaculoy[x]] = '#';
+									jogadory++;
+									break;
+								}
+							}
+							if (jogadory < 0) {
+								jogadory = 0;
+							}
+							if (parar == 1){
+								parar = 0;
+								break;
+							}
+
+
 						}
 						a++;
 						break;
@@ -71,6 +91,9 @@ int main(int argc, char** argv[]) {
 						for (p = 0; p < n; p++) {
 							campo[jogadory][jogadorx] = '1';
 							jogadory--;
+							if (jogadory < 0) {
+								jogadory = 0;
+							}
 						}
 						a++;
 						break;
@@ -78,6 +101,9 @@ int main(int argc, char** argv[]) {
 					for (p = 0; p < n; p++) {
 							campo[jogadory][jogadorx] = '1';
 							jogadory++;
+							if (jogadory > 9) {
+								jogadory = 9;
+							}
 						}
 						a++;
 						break;
